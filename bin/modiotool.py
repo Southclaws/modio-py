336861f5719eabf0d051ab5c638e3769d2984215
Southclaw <southclawjk@gmail.com>
2014-11-23 16:17:51 +0000

    Added file writing
    put method implemented
    
    Added data validation/conversion (strings inside cell lists are
    converted to sets of uints)

--- bin/modiotool.py
+++ bin/modiotool.py
@@ -7,7 +7,6 @@ import os
 
 
 def main():
-
 	logging.basicConfig(filename='modio_debug.log', level=logging.DEBUG)
 
 	parser = argparse.ArgumentParser(description='Read and write modio format files.')
@@ -27,7 +26,7 @@ def main():
 	args = parser.parse_args()
 
 	if args.input:
-		if os.path.splitext(args.input) == ".json":
+		if os.path.splitext(args.input)[1] == ".json":
 			print("input file is json, convert to modio")
 			readJSON(args.input, args.output, args.tags)
 
@@ -40,47 +39,69 @@ def main():
 
 
 def readJSON(inp, out, tags):
+	data = {}
 
-	print("Reading JSON...")
+	with io.open(inp, "r") as f:
+		data = json.loads(f.read())
 
 	if not out:
-		print("Dumping JSON to console...")
+		for num, k in enumerate(data):
+			if tags:
+				if k in tags:
+					print("[%d/%d] tag:'%s',data:'%s'"%(num+1, len(data), k, data[k]))
+
+			else:
+				print("[%d/%d] tag:'%s',data:'%s'"%(num+1, len(data), k, data[k]))
 
 	else:
 		print("Writing JSON data to modio output...")
 
-		# m = modio.open("file.dat", modio.mode_write)
-		# m.put("DATA", data)
-		# m.close()
+		with modio.open(out, modio.mode_write) as m:
+			for num, k in enumerate(data):
+				if tags:
+					if k in tags:
+						m.put(k, data[k])
+
+				else:
+					m.put(k, data[k])
 
 
 def readModio(inp, out, tags):
-
 	data = []
 	length = 0
 
+	version = 0
+	filesize = 0
+	numtags = 0
+
 	if tags:
-		with modio.open(inp, 0) as m:
+		with modio.open(inp, modio.mode_read) as m:
+			version, filesize, numtags = m.metadata()
 			for t in tags:
 				data.append(m.get(t))
 
 	else:
-		with modio.open(inp, 0) as m:
+		with modio.open(inp, modio.mode_read) as m:
+			version, filesize, numtags = m.metadata()
 			data = m.get()
 
 	if not out:
+		print("version:", version)
+		print("file_size:", filesize)
+		print("num_tags:", numtags)
 		for num, i in enumerate(data):
-			print("[%d/%d] tag:'%s',size:'%d',data:'%s'"%(num+1, len(data), i.getNameStr(), i.getSize(), i.getDataFormat()))
+			print("[%d/%d] tag:'%s',size:'%d',data:'%s'"%(num+1, len(data), i.get_name_str(), i.get_size(), i.get_data_format()))
 
 	else:
-		serialised = {}
+		print("Writing modio data to JSON output...")
+
+		data_dict = {}
 
 		for i in data:
-			serialised[i.getNameStr()] = i.getDataFormat()
+			data_dict[i.get_name_str()] = i.get_data_format()
 
 		with io.open(out, "w") as f:
-			f.write(json.dumps(serialised, indent=4))
-
+			f.write(json.dumps(data_dict, indent=4))
 
 
 if __name__ == '__main__':
--- pymodiolib/__init__.py
+++ pymodiolib/__init__.py
@@ -4,25 +4,45 @@ import string
 import logging
 
 
+version = 20
+mode_read = 0
+mode_write = 1
+
+
 class ModioFileTag:
 
 	def __init__(self, tag, data):
-		self.tag = tag
-		self.data = data
+		validate_data_block_types(data)
 
-	def getName(self):
+		if type(tag) is str:
+			self.tag = _tag_StringToInt(tag)
+			self.data = data
+
+		else:
+			self.tag = tag
+			self.data = data
+
+	def get_name(self):
 		return self.tag
 
-	def getData(self):
+	def get_data(self):
 		return self.data
 
-	def getSize(self):
+	def get_size(self):
 		return len(self.data)
 
-	def getNameStr(self):
+	def get_total_size(self):
+		return len(self.data) + 2
+
+	def get_name_str(self):
 		return _tag_IntToString(self.tag)
 
-	def getDataFormat(self):
+	def get_data_block(self):
+		result = [self.tag, len(self.data)]
+		result.extend(self.data)
+		return result
+
+	def get_data_format(self):
 		data = []
 		tmp = ""
 		for i in self.data:
@@ -41,77 +61,179 @@ class ModioFileTag:
 
 class ModioSession:
 
-	def __init__(self, filename):
+	# Attributes for a modio object:
+
+	# self.filename
+	# self.filemode
+	# self.filever
+	# self.filelen
+	# self.numtags
+
+	# self.filedata
+	# self.tagdict
+
+	def __init__(self, filename, filemode):
 		logging.debug("__init__: %s"%(self))
 
 		self.filename = filename
+		self.filemode = filemode
 
 	def __enter__(self):
 		logging.debug("__enter__: %s"%(self))
 
-		self.readStack = []
+		self.filedata = []
 
 		self.filever = 0
 		self.filelen = 0
 		self.numtags = 0
-		self.taglist = {}
+		self.tagdict = {}
 
-		try:
-			filehandle = io.open(self.filename, "rb")
+		if self.filemode == mode_read:
+			return self.__read__()
 
-		except ValueError:
-			return 0
+		elif self.filemode == mode_write:
+			return self.__write__()
 
-		filedata = filehandle.read()
-		fmat = "<%dI" % (len(filedata) // 4)
-		self.readStack = list(struct.unpack(fmat, filedata))
-
-		filehandle.close()
-
-		self.filever = self.readStack[0]
-		self.filelen = self.readStack[1]
-		self.numtags = self.readStack[2]
-
-		head_pointer = 3
-
-		for i in range(self.numtags):
-
-			tagname = self.readStack[head_pointer]
-			physpos = self.readStack[head_pointer + 1]
-			tagsize = self.readStack[3 + (self.numtags * 2) + physpos + 1]
-			tagdata = self.readStack[3 + (self.numtags * 2) + physpos + 2 : (3 + (self.numtags * 2) + physpos + 2 + tagsize)]
-
-			self.taglist[_tag_IntToString(tagname)] = ModioFileTag(tagname, tagdata)
-
-			head_pointer += 2
-
-		return self
+		return
 
 	def __exit__(self, *err):
 		logging.debug("__exit__: %s, %s"%(self, err))
 		self.close()
 
+	def __read__(self):
+		logging.debug("__read__")
+
+		try:
+			filehandle = io.open(self.filename, "rb")
+
+		except FileNotFoundError:
+			return 0
+
+		filedata = filehandle.read()
+		fmat = "<%dI" % (len(filedata) // 4)
+		self.filedata = list(struct.unpack(fmat, filedata))
+
+		filehandle.close()
+
+		self.filever = self.filedata[0]
+		self.filelen = self.filedata[1]
+		self.numtags = self.filedata[2]
+
+		head_pointer = 3
+
+		for i in range(self.numtags):
+
+			tagname = self.filedata[head_pointer]
+			physpos = self.filedata[head_pointer + 1]
+			tagsize = self.filedata[3 + (self.numtags * 2) + physpos + 1]
+			tagdata = self.filedata[3 + (self.numtags * 2) + physpos + 2 : (3 + (self.numtags * 2) + physpos + 2 + tagsize)]
+
+			self.tagdict[_tag_IntToString(tagname)] = ModioFileTag(tagname, tagdata)
+
+			head_pointer += 2
+
+		return self
+
+	def __write__(self):
+		logging.debug("__write__")
+
+		return self
+
 	def close(self):
 		logging.debug("close: %s"%(self))
+
+		if self.filemode == mode_write:
+
+			with io.open(self.filename, "wb") as f:
+				data = self.get_bytes()
+
+				for i, j in enumerate(data):
+					#print("[%02d/%02d] : %d"%(i, len(data), data[i]))
+					print(i, type(data[i]), data[i])
+
+				print(struct.pack('%dI'%(len(data)), *data))
+
+				f.write(struct.pack('%dI'%(len(data)), *data))
+
 		return
+
+	def get_bytes(self):
+		# 3 cells for the metadata, then tag count x 2 (tag name + tag physpos)
+		header_size = 3 + (len(self.tagdict) * 2)
+		body_size = 0
+
+		# version, file length and tag count (file length is added afterwards)
+		header = [version, 0, len(self.tagdict)]
+		body = []
+
+		# Points at the physical position of the current tag data block
+		header_physpos_pointer = 0
+
+		# Stores the size of the current tag data block (including metadata)
+		tag_size = 0
+
+		for tag in self.tagdict.values():
+			print("APPENDING TAG", tag.get_name(), tag.get_name_str(), tag.get_data())
+			# body size = sum of tag total sizes (tag + n + n data cells)
+			tag_size = tag.get_total_size()
+			body_size += tag_size
+
+			print("TAG SIZE: ", tag_size)
+
+			# Append two cells to the header (the tag name and the physical pos)
+			header += [tag.get_name(), header_physpos_pointer]
+
+			# Increment the header physical position pointer by the data size
+			header_physpos_pointer += tag_size
+
+			# Append the actual data block to the body (including metadata)
+			body.extend(tag.get_data_block())
+
+		header[1] = header_size + body_size
+
+		print("HEADER SIZE: %d BODY SIZE: %d TOTAL: %d"%(header_size, body_size, header_size+body_size))
+
+		return header + body
 
 	def get(self, tag = ""):
 		logging.debug("get: %s, %s"%(self, tag))
 
 		if tag:
-			return self.taglist[tag]
+			return self.tagdict[tag]
 
 		else:
-			return list(self.taglist.values())
+			return list(self.tagdict.values())
 
-	def put(self, tag):
+	def put(self, tag, data):
 		logging.debug("put: %s, %s"%(self, tag))
+
+		self.tagdict[tag] = ModioFileTag(tag, data)
+
 		return
 
+	def metadata(self):
+		return self.filever, self.filelen, self.numtags
 
-def open(filename, mode):
-	m = ModioSession(filename)
+	def version(self):
+		return self.filever
+
+	def file_size(self):
+		return self.filelen
+
+	def num_tags(self):
+		return self.numtags
+
+
+
+def open(filename, filemode):
+	m = ModioSession(filename, filemode)
 	return m
+
+
+#
+#	Utility functions
+#
+
 
 def _tag_StringToInt(s):
 	if len(s) != 4:
@@ -128,3 +250,24 @@ def _tag_IntToString(i):
 	s.append(str(chr(i & 0xFF)))
 
 	return "".join(s)
+
+# Ensures all elements in 'data' are uints
+def validate_data_block_types(data):
+	for i, d in enumerate(data):
+		datatype = type(d)
+
+		# If the data type is a string, insert each char as a uint
+		if datatype == str:
+			# Delete the existing element (the string)
+			del(data[i])
+
+			# Run through the string, grab each character integer and insert it
+			pos = 0
+			for char in d:
+				data.insert(i + pos, ord(char))
+				pos += 1
+
+		elif datatype != int:
+			return 0
+
+	return 1
